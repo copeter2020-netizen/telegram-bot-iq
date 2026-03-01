542e04e83ea6dff2cfd8beed1ccde4007890b2dd
copeter2020-netizen <copeter2020@gmail.com>
2026-03-01 16:07:36 -0500

    Update iq_connector.py

--- iq_connector.py
+++ iq_connector.py
@@ -1,1 +1,22 @@
+import time
+from iqoptionapi.stable_api import IQ_Option
 
+class IQConnector:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.api = IQ_Option(email, password)
+
+    def connect(self):
+        check, reason = self.api.connect()
+        if check:
+            self.api.change_balance("PRACTICE")
+            return True
+        else:
+            print("Error conectando:", reason)
+            return False
+
+    def get_candles(self, pair="EURUSD-OTC", timeframe=60, count=100):
+        end_from_time = time.time()
+        candles = self.api.get_candles(pair, timeframe, count, end_from_time)
+        return candles
